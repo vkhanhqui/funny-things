8a28c94bf6f9b4d419ee6ecd7396be9463f04702
Qui Vo Khanh <vkhanhqui@gmail.com>
2025-06-08 21:54:20 +0700

    Create simple REPL

---
+++ 014-interpreter/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"interpreter/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
--- 014-interpreter/repl/repl.go
+++ 014-interpreter/repl/repl.go
@@ -1,1 +1,30 @@
 package repl
+
+import (
+	"bufio"
+	"fmt"
+	"interpreter/lexer"
+	"interpreter/token"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
